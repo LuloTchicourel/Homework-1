535897655e1584ab3d7c62deb75fb98ed1ec0d80
LuloTchi <ltchicourel@udesa.edu.ar>
2025-03-19 13:41:02 -0300

    punto 2 completo

--- error_logs/README.md
+++ error_logs/README.md
@@ -1,11 +1,6 @@
-Para compilar el 2a:
-g++ log_message_2a.cpp -Wall -g -o logs2a      
+Para compilar:
+g++ log_message.cpp -Wall -g -o logs   
 
-Para correr el 2a:
-./logs2a
+Para correr:
+./logs
 
-Para compilar el 2b:
-g++ log_message_2b.cpp -Wall -g -o log2b  
-
-Para correr el 2b:
-./logs2b
--- error_logs/log_message.cpp
+++ error_logs/log_message.cpp
@@ -35,12 +35,41 @@ void log_message(string message, levels level){
     }
 }
 
+void log_message(string msg, string file, int line){
+    ofstream outFile(file, ios::app);
+    outFile << msg << " in line: " << line << endl;
+    outFile.close();
+}
+
+void log_message(string msg_access, string username){
+    ofstream outFile("logs_file.txt", ios::app);
+    outFile << "[SECURITY] " << msg_access << "user: " << username << endl;
+    outFile.close();
+}
+
 int main(){
     log_message("Se accedio a memoria invalida", levels::CRITICAL);
     log_message("Se freno en la linea 5", levels::DEBUG);
     log_message("No se puede restar un tipo list con un tipo map", levels::ERROR);
     log_message("Casteo a int puede hacer perder precision del float", levels::WARNING);
     log_message("El archivo de salida se creo con exito", levels::INFO);
+    try{
+        float a, b;
+        cout << "Choose a: "; cin >> a;
+        cout << "Choose b: "; cin >> b; cout << endl;
+        cout << "Doing a/b..." << endl;
+        if (b == 0){
+            throw runtime_error("Division by 0 was attempted");
+        }
+        float result = a/b;
+        cout << "Result: " << result << endl;
+    } 
+    catch (exception &e){
+        log_message(e.what(), levels::ERROR);
+        log_message(e.what(), "logs_file.txt", 64);
+        cout << "Loggin out with error " << e.what() << endl;
+        exit(1);
+    }
     return 0;
 }
 
