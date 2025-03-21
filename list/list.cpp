23d88d80d56fea754ad420866b2af7346af93c01
LuloTchi <ltchicourel@udesa.edu.ar>
2025-03-20 23:38:51 -0300

    me salio de taquito el 3

--- list/list.cpp
+++ list/list.cpp
@@ -1,57 +1,40 @@
 #include <iostream>
-#include <memory>
+#include "list.h"
 
 using namespace std;
 
-struct Node {
-    shared_ptr<Node> next;
-    int value;
-};
+int main() {
+    List myList = {nullptr, nullptr, 0};
 
-struct List {
-    shared_ptr<Node> head;
-    shared_ptr<Node> tail;
-    u_int8_t size;
-};
+    cout << "\nInitial list:\n" << endl;
+    print_list(&myList);
 
-shared_ptr<Node> create_node(int value){
-    shared_ptr<Node> node = make_shared<Node>();
-    node->next = nullptr;
-    node->value = value;
-    return node;
-}
+    cout << "\nTesting push_front() and push_back():" << endl;
+    push_front(&myList, 10);
+    print_list(&myList);
+    push_back(&myList, 20);
+    print_list(&myList);
+    push_back(&myList, 30);
+    print_list(&myList);
+    push_front(&myList, 5);
+    print_list(&myList);
 
-void push_front(List *list, int value){
-    shared_ptr<Node> new_node = create_node(value);
-    new_node->next = list->head;
-    list->head = new_node;
-    list->size++;
-    return;
-}
+    cout << "\nTesting insert():" << endl;
+    insert(&myList, 15, 2); 
+    print_list(&myList);
+    insert(&myList, 1, 0); 
+    print_list(&myList);
+    insert(&myList, 40, 10);
+    print_list(&myList);
 
-void push_back(List *list, int value){
-    shared_ptr<Node> new_node = create_node(value);
-    new_node->next = list->head;
-    list->head = new_node;
-    list->size++;
-    return;
-}
+    cout << "\nTesting erase():" << endl;
+    erase(&myList, 2);  
+    print_list(&myList);
+    erase(&myList, 0);  
+    print_list(&myList);
+    erase(&myList, 10); 
+    print_list(&myList);
 
-void insert(List *list, int value, int pos){
-    if (pos + 1 > list->size){
-        list->tail = create_node(value);
-        return;
-    }
-    shared_ptr<Node> curr = list->head;
-    for (int i = 0; i <= pos; i++){
-        curr = curr->next;
-    }    
-}
-
-void erase(){
-
-}
-
-void print_list(){
-
+    cout << "\nAll tests have been completed." << endl;
+    return 0;
 }
---
+++ list/list.h
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+struct Node {
+    shared_ptr<Node> next;
+    int value;
+};
+
+struct List {
+    shared_ptr<Node> head;
+    shared_ptr<Node> tail;
+    u_int8_t size;
+};
+
+shared_ptr<Node> create_node(int value){
+    shared_ptr<Node> node = make_shared<Node>();
+    node->next = nullptr;
+    node->value = value;
+    return node;
+}
+
+void push_front(List *list, int value){
+    shared_ptr<Node> new_node = create_node(value);
+    new_node->next = list->head;
+    list->head = new_node;
+    if (list->size == 0) list->tail = new_node;
+    list->size++;
+    return;
+}
+
+void push_back(List *list, int value){
+    shared_ptr<Node> new_node = create_node(value);
+    if (list->size == 0){
+        list->head = new_node;
+        list->tail = new_node;
+    }
+    else {
+        list->tail->next = new_node;
+        list->tail = new_node;
+    }
+    list->size++;
+    return;
+}
+
+void insert(List *list, int value, int pos){
+    if (pos + 1 > list->size){
+        cout << "Position exceeds length of list, value has been pushed back." << endl;
+        push_back(list, value);
+        return;
+    }
+
+    if (pos == 0) {push_front(list, value); return;}
+
+    shared_ptr<Node> curr = list->head;
+    for (int i = 0; i < pos - 1; i++){
+        curr = curr->next;
+    }    
+
+    shared_ptr<Node> new_node = create_node(value);
+    new_node->next = curr->next;
+    curr->next = new_node;
+    list->size++;
+    return;
+}
+
+void erase(List *list, int pos) {
+    if (list->size == 0) {
+        cout << "List is empty, there is nothing to erase." << endl;
+        return;
+    }
+    if (pos + 1 > list->size) {
+        cout << "Position exceeds length of list, erasing last node." << endl;
+        pos = list->size - 1;
+    }
+    if (pos == 0) {
+        list->head = list->head->next; 
+        list->size--;
+        if (list->size == 0) list->tail = nullptr;
+        return;
+    }
+    shared_ptr<Node> curr = list->head;
+    for (int i = 0; i < pos - 1; i++) curr = curr->next;
+    shared_ptr<Node> temp = curr->next;
+    curr->next = temp->next; 
+    if (pos == list->size - 1) list->tail = curr;
+    list->size--;
+}
+
+void print_list(const List *list) {
+    if (!list->head) {
+        cout << "List is empty." << endl;
+        return;
+    }
+    shared_ptr<Node> temp = list->head;
+    while (temp) {
+        cout << temp->value;
+        if (temp->next) cout << " -> "; 
+        temp = temp->next;
+    }
+    cout << " -> NULL" << endl;
+}
